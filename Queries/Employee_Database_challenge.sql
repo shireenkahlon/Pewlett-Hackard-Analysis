e05243d874ebbb20971324254ba8c70b463917e0
shireenkahlon <75495212+shireenkahlon@users.noreply.github.com>
2021-02-06 19:06:16 -0600

    Update Employee_Database_challenge.sql

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -6,7 +6,7 @@ SELECT e.emp_no
 	, t.from_date
 	, t.to_date
 -- Create a table
--- INTO retirement_titles
+INTO retirement_titles
 -- from table employees
 FROM employees AS e
 -- inner join titles table
@@ -15,7 +15,7 @@ ON e.emp_no=t.emp_no
 -- filter out data between Jan 1st. 1952 and Dec. 31st 1955
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 -- sort by employee number
-ORDER BY e.emp_no
+ORDER BY e.emp_no;
 
 -- filter distinct columns in employee number, add employee number columns, first_name columns, last_name columns, title columns
 SELECT DISTINCT ON (rt.emp_no)
@@ -23,13 +23,12 @@ SELECT DISTINCT ON (rt.emp_no)
 	, rt.first_name
 	, rt.last_name
 	, rt.title
--- into table unique_titles
--- INTO unique_titles
+INTO unique_titles
 -- from retirement_titles table
 FROM retirement_titles AS rt
 -- sort employee number ascending, and to_date & title descending
 ORDER BY rt.emp_no
-	, (rt.to_date, title) DESC
+	, (rt.to_date, title) DESC;
 	
 -- count the number of titles from unique_tables, add title column
 SELECT COUNT(ut.title)
@@ -41,7 +40,7 @@ FROM unique_titles AS ut
 -- filter by title
 GROUP BY ut.title
 -- sort by the count of emp_no descending
-ORDER BY count(ut.emp_no) DESC
+ORDER BY count(ut.emp_no) DESC;
 
 -- filter distinct employee_number values, add employee_number, first_name, last_name, birth_date, from_date, to_date, and title columns
 SELECT DISTINCT ON (e.emp_no)
@@ -53,7 +52,7 @@ SELECT DISTINCT ON (e.emp_no)
 	, de.to_date
 	, t.title
 -- Add data into a new table
--- INTO membership_eligibility
+INTO membership_eligibility
 -- inner join employees to dept_emp on primary keys and employees to titles on primary keys
 FROM employees AS e
 INNER JOIN dept_emp AS de
@@ -64,6 +63,6 @@ ON e.emp_no=t.emp_no
 WHERE (de.to_date = '9999-01-01') 
 	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 -- sort by emp_no
-ORDER BY e.emp_no
+ORDER BY e.emp_no;
 
 
